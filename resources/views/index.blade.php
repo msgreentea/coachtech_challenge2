70a292aafb712997cb2e89d84437a97767782057
msgreentea <msgreentea8@gmail.com>
2021-12-30 14:39:57 +0900

    still editing html

---
+++ resources/views/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layout')
+
+@section('title')
+    お問い合わせ
+@endsection
+
+
+@section('content')
+  <dl>
+    <dt>お名前<span>※</span></dt>
+    <dd></dd>
+    <dt>性別<span>※</span></dt>
+    <dd></dd>
+    <dt>メールアドレス<span>※</span></dt>
+    <dd></dd>
+    <dt>郵便番号<span>※</span></dt>
+    <dd></dd>
+    <dt>住所<span>※</span></dt>
+    <dd></dd>
+    <dt>建物名<span>※</span></dt>
+    <dd></dd>
+    <dt>ご意見<span>※</span></dt>
+    <dd></dd>
+  </dl>
+@endsection
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  @yield('css')
+  <link rel="stylesheet" href="share.css">
+  <link rel="stylesheet" href="{{ asset('/css/.css') }}">
+  <title>@yield('title')</title>
+</head>
+<body>
+  @yield('title')
+  @yield('content')
+</body>
+</html>
